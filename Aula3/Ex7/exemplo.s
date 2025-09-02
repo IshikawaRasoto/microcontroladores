eb0ab7f9a9f1941a80639c0f591c1030dbd086b6
IshikawaRasoto <ishikawarasoto@gmail.com>
2025-09-01 22:50:19 -0300

    Aula3

---
+++ Aula3/Ex7/exemplo.s
@@ -0,0 +1,60 @@
+; Exemplo.s
+; Desenvolvido para a placa EK-TM4C1294XL
+; Prof. Guilherme Peron
+; 12/03/2018
+
+; -------------------------------------------------------------------------------
+        THUMB                        ; Instruções do tipo Thumb-2
+; -------------------------------------------------------------------------------
+; Declarações EQU - Defines
+;<NOME>         EQU <VALOR>
+; -------------------------------------------------------------------------------
+; Área de Dados - Declarações de variáveis
+		AREA  DATA, ALIGN=2
+		; Se alguma variável for chamada em outro arquivo
+		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
+		                                   ; partir de outro arquivo
+;<var>	SPACE <tam>                        ; Declara uma variável de nome <var>
+                                           ; de <tam> bytes a partir da primeira 
+                                           ; posição da RAM		
+
+; -------------------------------------------------------------------------------
+; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
+;                  código
+        AREA    |.text|, CODE, READONLY, ALIGN=2
+
+		; Se alguma função do arquivo for chamada em outro arquivo	
+        EXPORT Start                ; Permite chamar a função Start a partir de 
+			                        ; outro arquivo. No caso startup.s
+									
+		; Se chamar alguma função externa	
+        ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
+									; função <func>
+
+; -------------------------------------------------------------------------------
+; Função main()
+Start  
+; Comece o código aqui <======================================================
+
+	mov	r0, #10
+
+loop
+	add	r0, r0, #5
+	cmp r0, #50
+	bne	loop
+	b func1
+fim
+	nop
+	b fim
+	
+func1
+	mov r1, r0
+	cmp r1, #50
+	ite	LO
+		addlo r1, r1, #1
+		movhs r1, #-50
+	bx lr
+	
+
+    ALIGN                           ; garante que o fim da seção está alinhada 
+    END                             ; fim do arquivo
